b75973fe68c73bfa53992cfd3bdf0aeee33a5bfe
Nick Ruggero <druggero7@gmail.com>
2020-09-07 20:05:41 -0400

    Finished 10-B

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,30 +1,29 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   async createUser(req, res){
     try {
       const {firstName, lastName, password, email} = req.body
-
       const existentUser = await User.findOne({email})
 
       if (!existentUser) {
         const hashedPassword = await bcrypt.hash(password, 10)
-        const user = await User.create({
+        const userResponse = await User.create({
           firstName,
           lastName,
           email,
           password: hashedPassword
         })
-        
-        return res.json({
-          _id: user._id,
-          email: user.email,
-          firstName: user.firstName,
-          lastName: user.lastName
+
+        return jwt.sign({ user: userResponse }, 'secret', (err, token) => {
+          return res.json({
+            user: token,
+            user_id: userResponse._id
+          })
         })
       }
-
       return res.status(400).json({
         message: 'email/user already exists! do you want to login instead?'
       })
